6e2c9db138b86ef618635f82e56d35531713dc14
lifeng0402 <960158047@qq.com>
2022-12-19 18:23:40 +0800

    增加vue框架

---
+++ vue3-test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="欢迎来到菜鸟教程！"/>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  comments: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
