a78c0b3b86e40c9f2505fbd9d011594c0ae5757f
lifeng0402 <960158047@qq.com>
2023-01-15 23:34:47 +0800

    增加待处理任务功能

--- vue3-element-plus-test/src/views/HomeView.vue
+++ vue3-element-plus-test/src/views/HomeView.vue
@@ -1,6 +1,40 @@
 <template>
       <div>
-            首页
-            <el-button type="parimary">Parimary</el-button>
+            <form @submit.prevent="addTask">
+                  <span>新建任务:</span>
+                  <input v-model="taskText" placeholder="请输入任务...">
+                  <button>添加</button>
+            </form>
+            <ol>
+                  <li v-for="(item, index) in todos" :key="(item, index)">
+                        {{ item }}
+                        <button @click="remove(index)">删除任务</button>
+                        <hr />
+                  </li>
+            </ol>
       </div>
 </template>
+<script>
+export default {
+      data() {
+            return {
+                  todos: [],
+                  taskText: ""
+
+            }
+      },
+      methods: {
+            addTask() {
+                  if (this.taskText.length == 0) {
+                        alert("请输入任务")
+                        return
+                  }
+                  this.todos.push(this.taskText)
+                  this.taskText = ""
+            },
+            remove(index) {
+                  this.todos.splice(index, 1)
+            }
+      }
+}
+</script>
