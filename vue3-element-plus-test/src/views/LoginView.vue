6c54db185a4f8d4123da9af998856636212cab94
lifeng0402 <960158047@qq.com>
2023-01-31 00:25:13 +0800

    页面修改为3.0版本

--- vue3-element-plus-test/src/views/LoginView.vue
+++ vue3-element-plus-test/src/views/LoginView.vue
@@ -1,108 +1,106 @@
 <template>
-      <div id="login">
-            <div class="login-wrap">
-                  <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm" size="medium">
+      <div class="login">
+            <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" class="login-wrap">
 
-                        <el-form-item prop="pass">
-                              <label>邮箱</label>
-                              <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
-                        </el-form-item>
+                  <el-form-item prop="pass">
+                        <label>邮箱</label>
+                        <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
+                  </el-form-item>
 
-                        <el-form-item prop="checkPass">
-                              <label>账号</label>
-                              <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
-                        </el-form-item>
+                  <el-form-item prop="checkPass">
+                        <label>账号</label>
+                        <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
+                  </el-form-item>
 
-                        <el-form-item prop="age">
-                              <label>密码</label>
-                              <el-input v-model.number="ruleForm.age"></el-input>
-                        </el-form-item>
+                  <el-form-item prop="age">
+                        <label>密码</label>
+                        <el-input v-model.number="ruleForm.age" />
+                  </el-form-item>
 
-                        <el-form-item>
-                              <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-                              <el-button @click="resetForm('ruleForm')">重置</el-button>
-                        </el-form-item>
-                  </el-form>
-            </div>
+                  <el-form-item>
+                        <el-button type="primary" @click="submitForm(ruleFormRef)" class="block">登录</el-button>
+                  </el-form-item>
+            </el-form>
       </div>
 </template>
-<script>
-export default {
-      data() {
-            var checkAge = (rule, value, callback) => {
-                  if (!value) {
-                        return callback(new Error('年龄不能为空'));
-                  }
-                  setTimeout(() => {
-                        if (!Number.isInteger(value)) {
-                              callback(new Error('请输入数字值'));
-                        } else {
-                              if (value < 18) {
-                                    callback(new Error('必须年满18岁'));
-                              } else {
-                                    callback();
-                              }
-                        }
-                  }, 1000);
-            };
-            var validatePass = (rule, value, callback) => {
-                  if (value === '') {
-                        callback(new Error('请输入密码'));
+    
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import type { FormInstance } from 'element-plus'
+
+const ruleFormRef = ref<FormInstance>()
+
+const checkAge = (rule: any, value: any, callback: any) => {
+      if (!value) {
+            return callback(new Error('Please input the age'))
+      }
+      setTimeout(() => {
+            if (!Number.isInteger(value)) {
+                  callback(new Error('Please input digits'))
+            } else {
+                  if (value < 18) {
+                        callback(new Error('Age must be greater than 18'))
                   } else {
-                        if (this.ruleForm.checkPass !== '') {
-                              this.$refs.ruleForm.validateField('checkPass');
-                        }
-                        callback();
+                        callback()
                   }
-            };
-            var validatePass2 = (rule, value, callback) => {
-                  if (value === '') {
-                        callback(new Error('请再次输入密码'));
-                  } else if (value !== this.ruleForm.pass) {
-                        callback(new Error('两次输入密码不一致!'));
-                  } else {
-                        callback();
-                  }
-            };
-            return {
-                  ruleForm: {
-                        pass: '',
-                        checkPass: '',
-                        age: ''
-                  },
-                  rules: {
-                        pass: [
-                              { validator: validatePass, trigger: 'blur' }
-                        ],
-                        checkPass: [
-                              { validator: validatePass2, trigger: 'blur' }
-                        ],
-                        age: [
-                              { validator: checkAge, trigger: 'blur' }
-                        ]
-                  }
-            };
-      },
-      methods: {
-            submitForm(formName) {
-                  this.$refs[formName].validate((valid) => {
-                        if (valid) {
-                              alert('submit!');
-                        } else {
-                              console.log('error submit!!');
-                              return false;
-                        }
-                  });
-            },
-            resetForm(formName) {
-                  this.$refs[formName].resetFields();
             }
+      }, 1000)
+}
+
+const validatePass = (rule: any, value: any, callback: any) => {
+      if (value === '') {
+            callback(new Error('Please input the password'))
+      } else {
+            if (ruleForm.checkPass !== '') {
+                  if (!ruleFormRef.value) return
+                  ruleFormRef.value.validateField('checkPass', () => null)
+            }
+            callback()
       }
 }
+const validatePass2 = (rule: any, value: any, callback: any) => {
+      if (value === '') {
+            callback(new Error('Please input the password again'))
+      } else if (value !== ruleForm.pass) {
+            callback(new Error("Two inputs don't match!"))
+      } else {
+            callback()
+      }
+}
+
+const ruleForm = reactive({
+      pass: '',
+      checkPass: '',
+      age: '',
+})
+
+const rules = reactive({
+      pass: [{ validator: validatePass, trigger: 'blur' }],
+      checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+      age: [{ validator: checkAge, trigger: 'blur' }],
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+      if (!formEl) return
+      formEl.validate((valid) => {
+            if (valid) {
+                  console.log('submit!')
+            } else {
+                  console.log('error submit!')
+                  return false
+            }
+      })
+}
 </script>
+    
 <style scoped>
 .login-wrap {
       margin: auto;
       width: 330px;
+}
+
+.block {
+      width: 100%;
+      display: block;
 }
 </style>
