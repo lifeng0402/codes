cfff42a98ab30e1b91a11dd5e1b2eaf230cfea3f
lifeng0402 <960158047@qq.com>
2023-05-23 20:18:02 +0800

    增加react脚手架

---
+++ react-element-plus-test/src/index.js
@@ -0,0 +1,10 @@
+// 引入核心库
+import React from 'react';
+// 引用ReactDOM
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// 引用组件
+import App from './App';
+
+// 渲染App到页面
+ReactDOM.render(<App />, document.getElementById('root'));
