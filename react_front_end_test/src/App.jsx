2446c3e9c1804d129d9198a89dc003535ac30103
lifeng0402 <960158047@qq.com>
2023-08-11 19:37:28 +0800

    合并登录和注册页面

--- react_front_end_test/src/App.jsx
+++ react_front_end_test/src/App.jsx
@@ -1,18 +1,39 @@
-import React, { Component } from 'react'
-import Login from "./components/Login"
-import Register from "./components/Register"
-import './App.css'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import './App.css';
 
 
 export default class App extends Component {
 
   render() {
     return (
-      <div className='app-container'>
-        {/* <Login /> */}
-        <Register/>
-      </div>
+      <Router>
+        <div className='container'>
+          <Login/>
+          {/* <h2>Which body of water?</h2>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/register">
+                  <code>/register</code>
+                </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  <code>/login</code>
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
+          <Routes>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </Routes> */}
+        </div>
+      </Router>
     )
   }
 }
--- react_front_end_test/src/components/login/index.jsx
+++ react_front_end_test/src/components/login/index.jsx
@@ -11,27 +11,37 @@ export default class Login extends Component {
       console.log('Failed:', errorInfo);
     };
     return (
-      <div className='login-form'>
-        <Form name="basic" labelCol={{ span: 8, }} wrapperCol={{ span: 16 }} style={{ maxWidth: 600, }}
-          initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
-          <Form.Item label="用户名" name="username" rules={[{ required: true, message: '请输入用户名!', },]}>
-            <Input />
-          </Form.Item>
+      <div className='login'>
+        <div className='card'>
+          <div className='title'>
+            Tesing...
+            <span>一套敏捷的接口测试及测试用例管理平台</span>
+          </div>
+          <span className='login_btn'>
+            <Button type="dashed">登录</Button>
+          </span>
+          <span className='register_btn'>
+            <Button type="dashed">注册</Button>
+          </span>
+          <div className='login_input'>
+            <Form name="basic" labelCol={{ span: 8, }} wrapperCol={{ span: 16 }} style={{ maxWidth: 600, }}
+              onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
+              <Form.Item label="用户名" name="username" rules={[{ required: true, message: '请输入用户名!', },]}>
+                <Input />
+              </Form.Item>
 
-          <Form.Item label="密码" name="password" rules={[{ required: true, message: '请输入密码!', },]}>
-            <Input.Password />
-          </Form.Item>
+              <Form.Item label="密  码" name="password" rules={[{ required: true, message: '请输入密码!', },]}>
+                <Input.Password />
+              </Form.Item>
+            </Form>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                注册并登录
+              </Button>
+            </Form.Item>
+          </div>
+        </div>
 
-          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16, }}>
-            <Checkbox>记住密码</Checkbox>
-          </Form.Item>
-
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              登录
-            </Button>
-          </Form.Item>
-        </Form>
       </div>
     );
   }
