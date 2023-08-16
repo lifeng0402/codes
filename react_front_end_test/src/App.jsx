48ee332cb903833f5e773302d517a8a2ebf6a0da
lifeng0402 <960158047@qq.com>
2023-08-16 19:32:46 +0800

    合并登录和注册页面

--- react_front_end_test/src/App.jsx
+++ react_front_end_test/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Login from "./components/Login";
-import Register from "./components/Register";
+// import Register from "./components/Register";
 import './App.css';
 
 
@@ -9,31 +9,9 @@ export default class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div className='container'>
-          <Login/>
-          {/* <h2>Which body of water?</h2>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/register">
-                  <code>/register</code>
-                </Link>
-              </li>
-              <li>
-                <Link to="/login">
-                  <code>/login</code>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Routes>
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-          </Routes> */}
-        </div>
-      </Router>
+      <div>
+        <Login/>
+      </div>
     )
   }
 }
--- react_front_end_test/src/components/login/index.jsx
+++ react_front_end_test/src/components/login/index.jsx
@@ -1,48 +1,66 @@
 import React, { Component } from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Form, Input, Radio } from 'antd';
 import "./index.css"
 
 export default class Login extends Component {
+
+  from = React.createRef();
+
+  state = {
+    requiredMark: 'optional',
+    isLogin: true //初始化状态为登录
+  };
+
+  onRequiredTypeChange = ({ requiredMarkValue }) => {
+    this.setState({ "requiredMark": requiredMarkValue })
+  };
+
+  toggleLogin = () => {
+    this.setState(preState => ({ isLogin: !preState.isLogin }));
+  };
+
   render() {
-    const onFinish = (values) => {
-      console.log('Success:', values);
-    };
-    const onFinishFailed = (errorInfo) => {
-      console.log('Failed:', errorInfo);
-    };
+
+    const { requiredMark, isLogin } = this.state;
+
     return (
       <div className='login'>
         <div className='card'>
           <div className='title'>
-            Tesing...
-            <span>一套敏捷的接口测试及测试用例管理平台</span>
+            AgileTC
+            <span>一套敏捷的测试用例管理平台</span>
           </div>
-          <span className='login_btn'>
-            <Button type="dashed">登录</Button>
-          </span>
-          <span className='register_btn'>
-            <Button type="dashed">注册</Button>
-          </span>
-          <div className='login_input'>
-            <Form name="basic" labelCol={{ span: 8, }} wrapperCol={{ span: 16 }} style={{ maxWidth: 600, }}
-              onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
-              <Form.Item label="用户名" name="username" rules={[{ required: true, message: '请输入用户名!', },]}>
-                <Input />
-              </Form.Item>
-
-              <Form.Item label="密  码" name="password" rules={[{ required: true, message: '请输入密码!', },]}>
-                <Input.Password />
-              </Form.Item>
-            </Form>
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                注册并登录
+          <Form
+            form={this.form}
+            layout="vertical"
+            initialValues={{ requiredMarkValue: requiredMark }}
+            onValuesChange={this.onRequiredTypeChange}
+          >
+            <Form.Item name="requiredMarkValue" requiredMark={requiredMark}
+              onFinish={this.onFinish}>
+              <Radio.Group>
+                <Radio.Button className='radioButton' value="optional" onClick={this.toggleLogin} checked={isLogin}>
+                  登录
+                </Radio.Button>
+                <Radio.Button className='radioButton' value={false} onClick={this.toggleLogin} checked={!isLogin}>
+                  注册
+                </Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item name="username" rules={[{ required: true, message: "" }]} wrapperCol={{ span: 40 }}>
+              <Input placeholder="账号:" />
+            </Form.Item>
+            <Form.Item name="password" rules={[{ required: true, message: "" }]} wrapperCol={{ span: 40 }}>
+              <Input.Password placeholder="密码:" />
+            </Form.Item>
+            <Form.Item>
+              <Button className='loginButton' type="primary" htmlType="submit">
+                {isLogin ? "登录" : "注册并登录"}
               </Button>
             </Form.Item>
-          </div>
+          </Form>
         </div>
-
       </div>
-    );
+    )
   }
 }
