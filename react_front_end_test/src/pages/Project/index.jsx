b818fee04e4aa5dc02f7d1abfed5bd177d1d9ce6
lifeng0402 <960158047@qq.com>
2023-09-13 19:50:30 +0800

    增加项目列表

--- react_front_end_test/src/pages/Project/index.jsx
+++ react_front_end_test/src/pages/Project/index.jsx
@@ -1,79 +1,47 @@
 import React, { Component } from 'react'
-import { Divider, Radio, Table } from 'antd';
-
+import { Collapse, Button, Table } from 'antd';
 
 
 export default class Project extends Component {
 
-      state = {
-            columns: [
-                  {
-                        title: 'Name',
-                        dataIndex: 'name',
-                        render: (text) => <a>{text}</a>,
-                  },
-                  {
-                        title: 'Age',
-                        dataIndex: 'age',
-                  },
-                  {
-                        title: 'Address',
-                        dataIndex: 'address',
-                  },
-            ],
-            data: [
-                  {
-                        key: '1',
-                        name: 'John Brown',
-                        age: 32,
-                        address: 'New York No. 1 Lake Park',
-                  },
-                  {
-                        key: '2',
-                        name: 'Jim Green',
-                        age: 42,
-                        address: 'London No. 1 Lake Park',
-                  },
-                  {
-                        key: '3',
-                        name: 'Joe Black',
-                        age: 32,
-                        address: 'Sydney No. 1 Lake Park',
-                  },
-                  {
-                        key: '4',
-                        name: 'Disabled User',
-                        age: 99,
-                        address: 'Sydney No. 1 Lake Park',
-                  },
-            ]
-      }
-
-      rowSelection = {
-            onChange: (selectedRowKeys, selectedRows) => {
-                  console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-            },
-            getCheckboxProps: (record) => ({
-                  disabled: record.name === 'Disabled User',
-                  // Column configuration not to be checked
-                  name: record.name,
-            }),
-      };
-
-
+      onChange = (key) => { console.log(key) };
 
       render() {
-            const { columns, data } = this.state;
             return (
                   <div>
-                        <Radio.Group>
-                              <Radio value="checkbox">Checkbox</Radio>
-                              <Radio value="radio">radio</Radio>
-                        </Radio.Group>
-
-                        <Divider />
-
-                        <Table columns={columns} dataSource={data} />
+                        <Collapse accordion items={[{
+                              key: '1',
+                              label: <div>
+                                    <p>33333333333333333</p>
+                                    <Button>运行按钮</Button>
+                              </div>,
+                              children: <div>
+                                    <Table columns={[
+                                          {
+                                                title: 'Name',
+                                                dataIndex: 'name',
+                                                key: 'name',
+                                          },
+                                          {
+                                                title: 'Age',
+                                                dataIndex: 'age',
+                                                key: 'age',
+                                          },
+                                          {
+                                                title: 'Address',
+                                                dataIndex: 'address',
+                                                key: 'address',
+                                          },
+                                          {
+                                                title: 'Action',
+                                                dataIndex: 'action',
+                                                key: 'address',
+                                          }
+                                    ]}></Table>
+                                    <p>{555555}</p>
+                                    <Button>子任务运行按钮</Button>
+                              </div>,
+                        }]} />
                   </div>
             )
       }
